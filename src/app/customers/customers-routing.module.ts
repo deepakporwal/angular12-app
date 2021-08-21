1d2f3384e84b2248287c512522483e91c7b79710
DESKTOP-H6FUKM1\deepak <er.deepakporwal@gmail.com>
2021-08-21 15:36:14 +0530

    router navigate changes

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerManageComponent } from './customer-manage/customer-manage.component';
 import { CustomersComponent } from './customers.component';
 
-const routes: Routes = [{ path: '', component: CustomerListComponent }];
+const routes: Routes = [
+  { path: 'manage', component: CustomerManageComponent },
+  { path: '', component: CustomerListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
